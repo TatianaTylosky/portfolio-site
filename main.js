87903c6efe3f891c95fc38f8b651575dc593c8bd
Tatiana Tylosky <tatianatylosky@gmail.com>
2014-08-25 11:08:40 -0400

    Nav loads for each page

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ $( document ).ready(function() {
 	var x = 0;
 
 	var pictures_array = [
+		"http://38.media.tumblr.com/282fadab7d782edce9debf3872c00ef1/tumblr_n3tswomqPS1st5lhmo1_1280.jpg",
 		"http://31.media.tumblr.com/f6c67ec2821a91051e4175f8a102e1e2/tumblr_n6rzpcsMk41st5lhmo1_1280.jpg", 
 		"http://38.media.tumblr.com/7ec15487856dbf285d695e5a4f8e12ef/tumblr_n6rzwbv3ji1st5lhmo1_1280.jpg",
 		"http://38.media.tumblr.com/e6c1c887ba8e24a855351940bcd3c343/tumblr_n8zm44LBpl1st5lhmo1_1280.jpg",
 		"http://38.media.tumblr.com/63748da1fab7ccebe70355807587d659/tumblr_n6rz5eNBG51st5lhmo1_1280.jpg",
 		"http://37.media.tumblr.com/09e6de2da0cb27a3a9008dc046ffe072/tumblr_n6essmaWs11st5lhmo1_1280.jpg",
 		"http://33.media.tumblr.com/eceae637f1f8459d627ade02a17f5198/tumblr_n4cm36DV9F1st5lhmo1_1280.jpg",
-		"http://38.media.tumblr.com/282fadab7d782edce9debf3872c00ef1/tumblr_n3tswomqPS1st5lhmo1_1280.jpg",
+		"http://38.media.tumblr.com/5c0a918c9f6b05db6002fb50cc0eacb8/tumblr_naj3mgttxL1st5lhmo1_1280.jpg",
+		"http://38.media.tumblr.com/5c0a918c9f6b05db6002fb50cc0eacb8/tumblr_naj3mgttxL1st5lhmo1_1280.jpg",
+		"http://31.media.tumblr.com/b92879483bd60c7db0871cc64b694b05/tumblr_n9hyqfJavs1st5lhmo1_1280.jpg",
 	];
 
 
@@ -34,5 +37,19 @@ $( document ).ready(function() {
 		// x = 0;
 		// debugger;
 	
+	$(".arrow").click(function() {
+  		console.log("You're scrolling!");
+  		window.scrollBy(0,20);
+	});
+
+
+
+	// function load_nav(){
+	// 	console.log("load nav did run")
+	// 	document.getElementById("nav").innerHtml.append("<object type="text/html" data="nav.html" ></object>");
+	// }
+
+	// load_nav();
+
 });
 
